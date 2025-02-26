9964fd4ffb1963ef104027040c2f2991d5602f53
Lazaro Makario <lazaro.makario@gmail.com>
2025-02-25 22:31:53 -0600

    ➕ (modularization): add flutter_modular and injectable

---
+++ app/lib/module.dart
@@ -0,0 +1,12 @@
+import 'package:cinemak/home.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+
+class AppModule extends Module {
+  @override
+  void binds(i) {}
+
+  @override
+  void routes(r) {
+    r.child('/', child: (context) => HomePage());
+  }
+}
